b9220d6f792245b30e9c721db3cd1735f59f6769
Svitlana Mykhailiuk <svetiklan@gmail.com>
2024-11-17 17:44:06 +0200

    store upd

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,6 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'root',
     storage,
-  whitelist: ['contacts'],
 };
 
 const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
